393fafddf30f8cf357dce8015943460e583acc52
prashanth <prashanth@shikshalokam.org>
2025-04-26 12:47:56 +0530

    removed is rubric checking condition

--- metabase-jobs/dashboard-creator/src/main/scala/org/shikshalokam/job/dashboard/creator/domain/Event.scala
+++ metabase-jobs/dashboard-creator/src/main/scala/org/shikshalokam/job/dashboard/creator/domain/Event.scala
@@ -3,7 +3,7 @@ package org.shikshalokam.job.dashboard.creator.domain
 import org.shikshalokam.job.domain.reader.JobRequest
 
 class Event(eventMap: java.util.Map[String, Any], partition: Int, offset: Long) extends JobRequest(eventMap, partition, offset) {
-
+  println(s"eventMap: $eventMap")
   def _id: String = readOrDefault[String]("_id", "")
 
   def reportType: String = readOrDefault[String]("reportType", "")
