42a0ca8b706ba5b4de13f2146db9d5eb4ec41329
Arshad S <arshad@shikshalokam.org>
2025-10-08 18:58:00 +0530

    [1593 - 1595] New Flink Job For Mentoring Stream Processor

---
+++ stream-jobs/mentoring-stream-processor/src/main/scala/org/shikshalokam/job/mentoring/stream/processor/domain/Event.scala
@@ -0,0 +1,131 @@
+package org.shikshalokam.job.mentoring.stream.processor.domain
+
+import org.shikshalokam.job.domain.reader.JobRequest
+import java.sql.Timestamp
+import java.text.SimpleDateFormat
+import java.time.format.DateTimeFormatter
+import java.time.{Instant, OffsetDateTime}
+import scala.reflect.ClassTag
+
+class Event(eventMap: java.util.Map[String, Any], partition: Int, offset: Long)
+  extends JobRequest(eventMap, partition, offset) {
+
+  def eventType: String = readOrDefault[String]("eventType", null)
+
+  def entity: String = readOrDefault[String]("entity", null)
+
+  def name: String = readOrDefault[String]("name", null)
+
+  def username: String = readOrDefault[String]("username", null)
+
+  def status: String = readOrDefault[String]("status", null)
+
+  def createdBy: String = extractValue[String]("created_by").orNull
+
+  def updatedBy: String = readOrDefault[String]("updated_by", null)
+
+  def id: Int = extractValue[Int]("id").getOrElse(-1)
+
+  def tenantCode: String = extractValue[String]("tenant_code").orNull
+
+  def createdAt: Timestamp = parseTimestamp(extractValue[Any]("created_at").orNull)
+
+  def updatedAt: Timestamp = parseTimestamp(extractValue[Any]("updated_at").orNull)
+
+  def deletedAt: Timestamp = parseTimestamp(extractValue[Any]("deleted_at").orNull)
+
+  def isDeleted: Boolean = extractValue[Boolean]("deleted").getOrElse(false)
+
+  def sessionId: Int = extractValue[Int]("session_id").getOrElse(-1)
+
+  def mentorId: String = extractValue[String]("mentor_id").orNull
+
+  def sessionName: String = extractValue[String]("name").orNull
+
+  def sessionDes: String = extractValue[String]("description").orNull
+
+  def sessionType: String = extractValue[String]("type").orNull
+
+  def sessionStatus: String = extractValue[String]("status").orNull
+
+  def startedAt: Timestamp = parseTimestamp(extractValue[Any]("started_at").orNull)
+
+  def completedAt: Timestamp = parseTimestamp(extractValue[Any]("completed_at").orNull)
+
+  def recommendedFor: String = extractValue[Seq[String]]("recommended_for").map(_.mkString(",")).getOrElse("")
+
+  def categories: String = extractValue[Seq[String]]("categories").map(_.mkString(",")).getOrElse("")
+
+  def medium: String = extractValue[Seq[String]]("medium").map(_.mkString(",")).getOrElse("")
+
+  def attId: Int = extractValue[Int]("att_id").getOrElse(-1)
+
+  def atSessionId: Int = extractValue[Int]("session_id").getOrElse(-1)
+
+  def menteeId: String = extractValue[String]("mentee_id").orNull
+
+  def joinedAt: Timestamp = parseTimestamp(extractValue[Any]("joined_at").orNull)
+
+  def leftAt: Timestamp = parseTimestamp(extractValue[Any]("left_at").orNull)
+
+  def isFeedbackSkipped: Boolean = extractValue[Boolean]("is_feedback_skipped").getOrElse(false)
+
+  def connId: Int = extractValue[Int]("conn_id").getOrElse(-1)
+
+  def userId: String = extractValue[String]("user_id").orNull
+
+  def friendId: String = extractValue[String]("friend_id").orNull
+
+  def orgId: String = extractValue[String]("org_id").orNull
+
+  def orgName: String = extractValue[String]("org_name").orNull
+
+  def orgCode: String = extractValue[String]("org_code").orNull
+
+  def platform: String = extractValue[String]("platform").orNull
+
+  def rating: Int = extractValue[Int]("rating").getOrElse(-1)
+
+  def ratingUpdatedAt: Timestamp = parseTimestamp(extractValue[Any]("rating_updated_at").orNull)
+
+  def userIdRoles: Int = extractValue[Int]("userId").getOrElse(-1)
+
+  def organizations: Seq[Map[String, Any]] = extractValue[Seq[Map[String, Any]]]("organizations").getOrElse(Seq.empty)
+
+  private val isUpdateEvent: Boolean = eventType == "update" || eventType == "bulk-update"
+
+  private def extractValue[T](key: String)(implicit ct: ClassTag[T]): Option[T] = {
+    def readKey(k: String): Option[Any] = Option(readOrDefault[Any](k, null.asInstanceOf[Any]))
+
+    val direct = readKey(key)
+    val fromNew = if (isUpdateEvent) readKey(s"newValues.$key") else None
+    val fromOld = if (isUpdateEvent) readKey(s"oldValues.$key") else None
+
+    (direct orElse fromNew orElse fromOld)
+      .filter(_ != null)
+      .map(_.asInstanceOf[T])
+  }
+
+  private def parseTimestamp(value: Any): Timestamp = value match {
+    case null => null
+    case ts: Timestamp => ts
+    case s: String if s.trim.nonEmpty =>
+      val str = s.trim
+      val parsers = Seq[(String => Option[Timestamp])](
+        in => scala.util.Try(Timestamp.valueOf(in)).toOption, //SQL timestamp format
+        in => scala.util.Try(Timestamp.from(Instant.parse(in))).toOption, //ISO-8601 UTC format
+        in => scala.util.Try { //ISO-8601 with offset
+          val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSSXXX")
+          val odt = OffsetDateTime.parse(in, formatter)
+          Timestamp.from(odt.toInstant)
+        }.toOption,
+        in => scala.util.Try { // Fallback format without millis or TZ
+          val formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+          new Timestamp(formatter.parse(in).getTime)
+        }.toOption
+      )
+      parsers.view.flatMap(parser => parser(str)).headOption.orNull
+    case _ => null
+  }
+
+}
---
+++ stream-jobs/mentoring-stream-processor/src/main/scala/org/shikshalokam/job/mentoring/stream/processor/task/MentoringStreamTask.scala
@@ -0,0 +1,54 @@
+package org.shikshalokam.job.mentoring.stream.processor.task
+
+import com.typesafe.config.ConfigFactory
+import org.apache.flink.api.common.typeinfo.TypeInformation
+import org.apache.flink.api.java.typeutils.TypeExtractor
+import org.apache.flink.api.java.utils.ParameterTool
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.shikshalokam.job.connector.FlinkKafkaConnector
+import org.shikshalokam.job.mentoring.stream.processor.domain.Event
+import org.shikshalokam.job.mentoring.stream.processor.functions.MentoringStreamFunction
+import org.shikshalokam.job.util.FlinkUtil
+
+import java.io.File
+
+class MentoringStreamTask(config: MentoringStreamConfig, kafkaConnector: FlinkKafkaConnector) {
+
+  private val serialVersionUID = -7729362727131516112L
+
+  def process(): Unit = {
+    implicit val env: StreamExecutionEnvironment = FlinkUtil.getExecutionContext(config)
+    implicit val eventTypeInfo: TypeInformation[Event] = TypeExtractor.getForClass(classOf[Event])
+    implicit val stringTypeInfo: TypeInformation[String] = TypeExtractor.getForClass(classOf[String])
+    val source = kafkaConnector.kafkaJobRequestSource[Event](config.inputTopic)
+
+    val progressStream = env.addSource(source).name(config.mentoringStreamConsumer)
+      .uid(config.mentoringStreamConsumer).setParallelism(config.kafkaConsumerParallelism)
+      .rebalance
+      .process(new MentoringStreamFunction(config))
+      .name(config.mentoringStreamFunction).uid(config.mentoringStreamFunction)
+      .setParallelism(config.mentoringStreamParallelism)
+
+    progressStream.getSideOutput(config.eventOutputTag)
+      .addSink(kafkaConnector.kafkaStringSink(config.outputTopic))
+      .name(config.metabaseDashboardProducer)
+      .uid(config.metabaseDashboardProducer)
+      .setParallelism(config.metabaseDashboardParallelism)
+
+    env.execute(config.jobName)
+  }
+}
+
+object MentoringStreamTask {
+  def main(args: Array[String]): Unit = {
+    println("Starting up the Mentoring Stream Job")
+    val configFilePath = Option(ParameterTool.fromArgs(args).get("config.file.path"))
+    val config = configFilePath.map {
+      path => ConfigFactory.parseFile(new File(path)).resolve()
+    }.getOrElse(ConfigFactory.load("mentoring-stream.conf").withFallback(ConfigFactory.systemEnvironment()))
+    val mentoringStreamConfig = new MentoringStreamConfig(config)
+    val kafkaUtil = new FlinkKafkaConnector(mentoringStreamConfig)
+    val task = new MentoringStreamTask(mentoringStreamConfig, kafkaUtil)
+    task.process()
+  }
+}
