7f8c9d6112042383e349e51896c9b61380fcd572
prashanth <prashanth@shikshalokam.org>
2025-11-05 15:28:56 +0530

    Updated the mentoring.py file to enable fetching of the config file

--- Documentation/batch-scripts/mentoring.py
+++ Documentation/batch-scripts/mentoring.py
@@ -11,6 +11,9 @@ import configparser
 
 
 # Load config.ini
+base_dir = os.path.dirname(os.path.abspath(__file__))
+config_path = os.path.join(base_dir, "config.ini")
+
 config = configparser.ConfigParser()
 config.read("config.ini")
 
